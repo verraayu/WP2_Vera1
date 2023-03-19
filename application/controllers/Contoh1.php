76324e10f50a7dce304e4dd1c7db550076f56c70
verraayu <verrastch@gmail.com>
2023-03-19 17:37:53 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Vera Ayu Rizkiyani
+            Saya tinggal di daerah Jakarta
+            Olahraga yang saya sukai adalah
+            Berenang";
+    }
+}
